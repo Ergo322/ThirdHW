faf5e9080fdffa362a6d6ff73385e026282cd4e2
Ergo322 <Shikmen2@mail.ru>
2022-08-30 22:36:32 +0300

    task23

---
+++ Задача23/Program.cs
@@ -0,0 +1,16 @@
+﻿void KUB()
+{
+    Console.Clear();
+    Console.Write ("Введите число, таблицу кубов которого вы хотите увидеть ");
+    double X  = double.Parse (Console.ReadLine());
+    double P = 0;
+    int count = 1;
+    Console.Write($"{X} -> ");
+    while (count<=X)
+    {
+        P = Math.Pow(count,3);
+        Console.Write ($"{P},");
+        count ++;
+    }
+} 
+KUB();
